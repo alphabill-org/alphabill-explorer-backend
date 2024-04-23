045f3c3a1af777edf78542277f3d7371d26c4ca6
Alexander Khrushkov <a.khrushkov@gmail.com>
2024-04-23 21:31:48 +0300

    refactoring

--- types/block.go
+++ types/block.go
@@ -8,6 +8,13 @@ import (
 	"github.com/alphabill-org/alphabill/types"
 )
 
+type BlockInfo struct {
+	_                  struct{} `cbor:",toarray"`
+	Header             *types.Header
+	TxHashes           []string
+	UnicityCertificate *types.UnicityCertificate
+}
+
 func NewBlockInfo(b *types.Block) (*BlockInfo, error) {
 	if b == nil {
 		return nil, fmt.Errorf("block is nil")
