79c5dded9d78009611f472938f208708e16daba8
Alexander Khrushkov <a.khrushkov@gmail.com>
2024-04-24 15:54:33 +0300

    TxInfo TargetUnits => []string

--- types/tx.go
+++ types/tx.go
@@ -15,7 +15,7 @@ type TxInfo struct {
 	Timeout          uint64
 	PayloadType      string
 	Status           *types.TxStatus
-	TargetUnits      []types.UnitID
+	TargetUnits      []string
 	TransactionOrder *types.TransactionOrder
 	Fee              uint64
 }
@@ -25,16 +25,23 @@ func NewTxInfo(blockNo uint64, txRecord *types.TransactionRecord) (*TxInfo, erro
 		return nil, fmt.Errorf("transaction record is nil")
 	}
 	hashHex := hex.EncodeToString(txRecord.Hash(crypto.SHA256))
+
+	units := make([]string, 0, len(txRecord.ServerMetadata.TargetUnits))
+
+	for _, unit := range txRecord.ServerMetadata.TargetUnits {
+		unitString := unit.String()
+		units = append(units, unitString)
+	}
+
 	txInfo := &TxInfo{
 		Hash:             hashHex,
 		BlockNumber:      blockNo,
 		Timeout:          txRecord.TransactionOrder.Timeout(),
 		PayloadType:      txRecord.TransactionOrder.PayloadType(),
 		Status:           &txRecord.ServerMetadata.SuccessIndicator,
-		TargetUnits:      []types.UnitID{},
+		TargetUnits:      units,
 		TransactionOrder: txRecord.TransactionOrder,
 		Fee:              txRecord.ServerMetadata.GetActualFee(),
 	}
-	txInfo.TargetUnits = txRecord.ServerMetadata.TargetUnits
 	return txInfo, nil
 }
