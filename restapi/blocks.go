03e38ff120e2f0bc45fac99c3e82db3a890028fb
Alexander Khrushkov <a.khrushkov@gmail.com>
2024-04-30 20:45:00 +0300

    Update getBlock , Add swagger getBlock

--- restapi/blocks.go
+++ restapi/blocks.go
@@ -8,23 +8,43 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// @Summary Retrieve a block by block number or the latest block
+// @Description Retrieves a block using the block number provided as a path parameter or retrieves the latest block if no block number is specified.
+// @Tags Blocks
+// @Accept json
+// @Produce json
+// @Param blockNumber path int false "The block number to retrieve (optional; if not provided, the latest block is returned)"
+// @Success 200 {object} BlockInfo "Successfully retrieved the block"
+// @Failure 400 {object} string "Invalid block number format or other client error"
+// @Failure 404 {object} string "Block with the specified block number not found"
+// @Failure 500 {object} string "Internal server error, such as failure to load the last block number or to load the block from the service"
+// @Router /blocks/{blockNumber} [get]
 func (api *MoneyRestAPI) getBlock(w http.ResponseWriter, r *http.Request) {
+
+	var blockNumber uint64
+
 	vars := mux.Vars(r)
 	blockNumberStr, ok := vars["blockNumber"]
-	if !ok {
-		http.Error(w, "Missing 'blockNumber' variable in the URL", http.StatusBadRequest)
-		return
-	}
 
-	blockNumber, err := strconv.ParseUint(blockNumberStr, 10, 64)
-	if err != nil {
-		http.Error(w, "Invalid 'blockNumber' format", http.StatusBadRequest)
-		return
+	if ok {
+		var err error
+		blockNumber, err = strconv.ParseUint(blockNumberStr, 10, 64)
+		if err != nil {
+			api.rw.ErrorResponse(w, http.StatusBadRequest, fmt.Errorf("invalid 'blockNumber' format: %v", err))
+			return
+		}
+	} else {
+		var err error
+		blockNumber, err = api.Service.GetLastBlockNumber()
+		if err != nil {
+			api.rw.ErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("failed to load last block number: %v", err))
+			return
+		}
 	}
 
 	block, err := api.Service.GetBlock(blockNumber)
 	if err != nil {
-		api.rw.WriteErrorResponse(w, fmt.Errorf("failed to load block with block number %d : %w", blockNumber, err))
+		api.rw.ErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("failed to load block with block number %d: %v", blockNumber, err))
 		return
 	}
 
